0ddbb4fb97671d58e473a4f2075fa0f4b6c0d5bf
annakuzub <annakuzub@gmail.com>
2022-09-15 08:15:28 +0300

    Added github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
-function App() {
-  return <div className="App"> Weather App </div>;
+export default function App() {
+  return (
+    <div className="App">
+      {" "}
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Anna Kuzub and is{" "}
+        <a
+          href="https://github.com/annakuzub/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
