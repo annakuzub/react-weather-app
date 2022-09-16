b4123f9f75dc01a8dfc75e0eb814a99693c00964
annakuzub <annakuzub@gmail.com>
2022-09-16 17:26:07 +0300

    Added project styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         {" "}
-        <h1>Weather App</h1>
+        <h1></h1>
+        <Weather />
         <footer>
           This project was coded by Anna Kuzub and is{" "}
           <a
             href="https://github.com/annakuzub/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
